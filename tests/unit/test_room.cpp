39607b3429685800cfd8bfeff3cf846f5fc76b92
vincent-meurie <vincent.meurie--ichai@efrei.net>
2025-10-09 10:04:25 +0700

    [Room] Test Reward system (RED)
    - Test adding single and multiple rewards
    - Test rewards metadata (god names, quantities)
    - Test clearing rewards
    - Tests fail: reward API does not exist yet
    
    Tests: 11 passing, 16 total

--- tests/unit/test_room.cpp
+++ tests/unit/test_room.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "core/Biome.h"
+#include "core/Reward.h"
 #include "core/Room.h"
 
 /**
@@ -94,4 +95,59 @@ TEST(RoomMetadataTest, DefaultDifficultyIsOne) {
   Room room("room_01", Room::Type::Combat);
 
   EXPECT_FLOAT_EQ(room.GetDifficulty(), 1.0f);
+}
+
+/**
+ * Test Suite: Room Rewards
+ * Testing reward assignment and managemenent
+ */
+
+TEST(RoomRewardTest, CanAddSingleReward) {
+  Room room("room_01", Room::Type::Combat);
+  room.AddReward(Reward::Type::Boon);
+
+  auto rewards = room.GetRewards();
+  ASSERT_EQ(rewards.size(), 1);
+  EXPECT_EQ(rewards[0].type, Reward::Type::Boon);
+}
+
+TEST(RoomRewardTest, CanAddMultipleRewards) {
+  Room room("room_01", Room::Type::Combat);
+  room.AddReward(Reward::Type::Boon);
+  room.AddReward(Reward::Type::Gold);
+  room.AddReward(Reward::Type::Pom);
+
+  auto rewards = room.GetRewards();
+  EXPECT_EQ(rewards.size(), 3);
+}
+
+TEST(RoomRewardTest, CanAddRewardWithMetadata) {
+  Room room("room_01", Room::Type::Combat);
+
+  Reward::Data boonReward(Reward::Type::Boon, "Zeus");
+  room.AddReward(boonReward);
+
+  auto rewards = room.GetRewards();
+  ASSERT_EQ(rewards.size(), 1);
+  EXPECT_EQ(rewards[0].type, Reward::Type::Boon);
+  EXPECT_EQ(rewards[0].godName, "Zeus");
+}
+
+TEST(RoomRewardTest, CanClearRewards) {
+  Room room("room_01", Room::Type::Combat);
+  room.AddReward(Reward::Type::Boon);
+  room.AddReward(Reward::Type::Gold);
+
+  EXPECT_EQ(room.GetRewards().size(), 2);
+
+  room.ClearRewards();
+  EXPECT_EQ(room.GetRewards().size(), 0);
+}
+
+TEST(RoomRewardTest, BossRoomsShouldNotHaveRewards) {
+  Room room("boss_room", Room::Type::Boss);
+
+  // Boss rooms typically don't have preview rewards
+  // they drop rewards after the fight
+  EXPECT_TRUE(room.GetRewards().empty());
 }
