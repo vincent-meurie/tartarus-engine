3686bbbcda08b630ee4b3b53587b48e1680f7959
vincent-meurie <vincent.meurie--ichai@efrei.net>
2025-10-10 09:46:18 +0700

    [PathGen] Test path generator creation (RED)
    - Test basic PathGenerator construction
    - Test default configuration values
    - Test custom configuration setting
    - Tests fail: PathGenerator.h doesn't exist
    
    Tests: 33 passing, 3 failing

---
+++ tests/unit/test_path_generator.cpp
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+
+#include "../test_utils.h"
+#include "generation/PathGenerator.h"
+
+/**
+ * Test Suite: PathGenerator Creation
+ * Testing basic path generator setup and configuration
+ */
+
+TEST(PathGeneratorTest, CanCreateGenerator) {
+  TestUtils::SeededRandom rng(42);
+  PathGenerator generator(rng.GetEngine());
+
+  // Just verify it constructs without errors
+  SUCCEED();
+}
+
+TEST(PathGeneratorTest, HasDefaultConfiguration) {
+  TestUtils::SeededRandom rng(42);
+  PathGenerator generator(rng.GetEngine());
+
+  auto config = genrator.GetConfig();
+
+  // Hades has roughly 40-50 rooms per run
+  EXPECT_GE(config.minRooms, 30);
+  EXPECT_LE(config.maxRooms, 60);
+  EXPECT_GT(config.branchProbability, 0.0f);
+  EXPECT_LT(config.branchProbability, 1.0f);
+}
+
+TEST(PathGeneratorTest, CanSetCustomConfiguration) {
+  TestUtils::SeededRandom rng(42);
+  PathGenerator generator(rng.GetEngine());
+
+  PathGenerator::Config customConfig;
+  customConfig.minRooms = 10;
+  customConfig.maxRooms = 15;
+  customConfig.branchProbability = 0.5f
+
+                                   generator.SetConfig(customConfig);
+
+  auto retrievedConfig = generator.GetConfig();
+  EXPECT_EQ(retrievedConfig.minRooms, 10);
+  EXPECT_EQ(retrievedConfig.maxrooms, 15);
+  EXPECT_FLOAT_EQ(retrievedConfig.branchProbability, 0.5f)
+}
